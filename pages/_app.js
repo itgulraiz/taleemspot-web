a0a0ecaaac154d8ac9bfd703e6723ea639e15113
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-01 02:04:23 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,8 @@
-import '../styles/globals.css'; // Path is correct if styles/globals.css exists in root
-import 'tailwindcss/tailwind.css';
-import { useEffect } from 'react';
+import 'react-loading-skeleton/dist/skeleton.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Apply dark mode based on system preference
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, []);
-
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
