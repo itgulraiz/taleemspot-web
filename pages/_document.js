572e33e28b8d4d6846d2b91d60a4191114b8ff70
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-01 13:20:02 +0500

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-XXXXXXXXXXXXXXXX" />
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX" 
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
