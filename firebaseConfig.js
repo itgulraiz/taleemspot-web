feeb2486e8d42575c5ebbc7006ef4372624f787f
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-02 21:57:47 +0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,10 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID
 };
 
 // Initialize Firebase properly
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth = getAuth(app); // ADDED EXPORT
+export const db = getFirestore(app); // REMOVED auth parameter
