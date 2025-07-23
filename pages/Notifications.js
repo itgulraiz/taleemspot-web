9ddd0ef9b0d9c4c612f81c00ae69da8e95091e0f
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:33:27 +0500

    Create Notifications.js

---
+++ pages/Notifications.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function Notifications() {
+  return (
+    <div>
+      <h1>All New Notifications</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
