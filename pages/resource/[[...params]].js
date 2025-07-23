1c6ee03ba6360827b4b0f20e35c13ce859bc3fd4
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:52:51 +0500

    Update [[...params]].js

--- pages/resource/[[...params]].js
+++ pages/resource/[[...params]].js
@@ -1,1 +1,11 @@
 
+// pages/resource/[[...params]].js
+
+export default function DynamicResourcePage() {
+  return (
+    <div>
+      <h1>Dynamic Resource Page</h1>
+      <p>This is your fallback dynamic route page.</p>
+    </div>
+  );
+}
