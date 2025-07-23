be63ee4db0baa6559437350c98a259ffe5181dbb
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:22:45 +0500

    Create general-resources.js

---
+++ pages/general-resources.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function AllGeneralResources() {
+  return (
+    <div>
+      <h1>All General Resources</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
