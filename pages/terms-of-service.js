2ca81409344a4593d99d7044d39f4c160b58b8d7
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:32:13 +0500

    Create terms-of-service.js

---
+++ pages/terms-of-service.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function TermsOfServices() {
+  return (
+    <div>
+      <h1>All Terms Of Services</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
