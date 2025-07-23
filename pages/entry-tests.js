bab63eb999e4a12d000d0da54f7e7647a7e5fe72
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:21:41 +0500

    Create entry-tests.js

---
+++ pages/entry-tests.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function AllEntryTest() {
+  return (
+    <div>
+      <h1>All Entry Tests</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
