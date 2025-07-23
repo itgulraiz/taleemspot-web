f25aadce21f8fdef55e1a27ac496f09ae1f037c7
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:20:44 +0500

    Create all-university.js

---
+++ pages/all-university.js
@@ -0,0 +1,9 @@
+// pages/all-university.js
+export default function AllUniversity() {
+  return (
+    <div>
+      <h1>All University Data</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
