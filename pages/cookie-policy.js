9759766f2c0f4a9903a4097c70c06342d45c3825
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:34:47 +0500

    Create cookie-policy.js

---
+++ pages/cookie-policy.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function CookiePolicy() {
+  return (
+    <div>
+      <h1>All New cookie policy</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
