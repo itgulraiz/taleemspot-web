88849093b670ca0526a0a043228302d098e544a9
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:31:10 +0500

    Create privacy-policy.js

---
+++ pages/privacy-policy.js
@@ -0,0 +1,9 @@
+// pages/all-news.js
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <h1>All News Page</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
