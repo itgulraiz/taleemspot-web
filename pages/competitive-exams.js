4b75e11f5f206075f222f02800f132b75037489e
Gulraiz Hamza <it.gulraiz@gmail.com>
2025-07-23 12:19:24 +0500

    Create competitive-exams.js

---
+++ pages/competitive-exams.js
@@ -0,0 +1,9 @@
+// pages/competitive-exams.js
+export default function CompetitionExams() {
+  return (
+    <div>
+      <h1>All Competition Exams</h1>
+      {/* Add actual news listing later */}
+    </div>
+  );
+}
